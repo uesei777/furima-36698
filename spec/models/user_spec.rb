c45954d2fe3afc4fef81a28f7c7f28a11b2ed6f2
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-11-15 20:36:33 +0900

    パスワードに関するテストの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -110,9 +110,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
       end
 
-      it 'passwordが半角英数字混合でないと登録できない' do
+      it 'passwordが英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid. Include both letters and numbers"
+      end
+
+      it 'passwordが数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid. Include both letters and numbers"
+      end
+
+      it 'passwordが全角全角文字を含むと登録できない' do
+        @user.password = '000aaあ'
+        @user.password_confirmation = '000aaあ'
         @user.valid?
         expect(@user.errors.full_messages).to include "Password is invalid. Include both letters and numbers"
       end
