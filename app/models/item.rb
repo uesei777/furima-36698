1b064c7454e0d7101137b82a21a54cc80c93b63b
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-11-19 15:13:26 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  validates :product, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }
+
+  validates :category, :status, :delivery_fee, :prefecture, :days, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :write_last_name, presence: true
