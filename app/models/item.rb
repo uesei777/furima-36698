c330709756087f53ea4a0ae633e7477ca513fefa
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-11-24 23:41:38 +0900

    商品購入機能に関するビューの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   validates :product, presence: true
   validates :explanation, presence: true
   validates :price, presence: true, numericality: { in: 300..9999999 }
 
-  validates :category, :status, :delivery_fee, :prefecture, :days, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, numericality: { other_than: 1, message: "can't be blank"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
