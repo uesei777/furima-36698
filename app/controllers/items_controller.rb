6a765aa6469fbdcd573059f2264826e39669dbda
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-12-10 14:07:07 +0900

    商品編集画面へのアクセスの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  
 
   def index
     @items = Item.order('created_at DESC')
@@ -24,6 +25,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
