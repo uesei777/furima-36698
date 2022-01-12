039f4fbb626f3fb450640f3725977ef8bd228032
uesei777 <sreiibyoa.1227@i.softbank.jp>
2022-01-12 23:16:28 +0900

    Formオブジェクトの作成

---
+++ app/models/order_information.rb
@@ -0,0 +1,17 @@
+class OrderInformation
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :phone_number, numericality: {only_integer: true}, length: { in: 10..11 }
+    validates :user_id, :item_id, :city, :address
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Information.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, order_id: order.id)
+  end
+end
