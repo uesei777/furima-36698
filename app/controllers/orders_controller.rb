fa4f8804aa84affc54062d9e1c7482e0afc6cfc5
uesei777 <sreiibyoa.1227@i.softbank.jp>
2022-01-16 14:42:47 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,21 @@
 class OrdersController < ApplicationController
   def index
-
+    @order_information = OrderInformation.new
   end
-  
+
   def create
-    
+    @order_information = OrderInformation.new(order_params)
+    if @order_information.valid?
+      @order_information.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_information).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_information.rb
+++ app/models/order_information.rb
@@ -12,6 +12,6 @@ class OrderInformation
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Information.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, order_id: order.id)
+    Information.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
